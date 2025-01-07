5c9ba38f62add2f82adc42f6b83733099b02266e
murphsj2 <sam.murphy.cw@jhuapl.edu>
2025-01-07 14:48:48 -0500

    untracks build folder

--- out/build/default/CMakeFiles/aspireTerminalPrototype.dir/src/TerminalCharacter.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/aspireTerminalPrototype.dir/src/TerminalCharacter.cpp.o: \
- /home/murphsj/aspire/qt-test/aspireTerminalPrototype/src/TerminalCharacter.cpp \
- /usr/include/stdc-predef.h
--- out/build/default/CMakeFiles/aspireTerminalPrototype_autogen_timestamp_deps.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/aspireTerminalPrototype_autogen_timestamp_deps"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/aspireTerminalPrototype_autogen_timestamp_deps.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
